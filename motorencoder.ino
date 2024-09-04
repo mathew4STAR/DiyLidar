d3fb06e3b1e08eafaa1210423557411105b57a89
Mathew <82754813+mathew4STAR@users.noreply.github.com>
2024-09-04 18:43:03 +0530

    Add files via upload

---
+++ motorencoder.ino
@@ -0,0 +1,132 @@
+#include <Wire.h>
+#include <AS5600.h>
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+  #define SERIAL SerialUSB
+  #define SYS_VOL   3.3
+#else
+  #define SERIAL Serial
+  #define SYS_VOL   5
+#endif
+
+AMS_5600 ams5600;
+
+int ang, lang = 0;
+
+#define STEPPER_PIN_1 9
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 11
+#define STEPPER_PIN_4 12
+
+int step_number = 0;
+float angle = 0;
+float step_angle = 0.17;
+int target_angle  = 90;
+bool go = true;
+int actual_step = 0;
+
+void setup()
+{
+
+  SERIAL.begin(115200);
+  Wire.begin();
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+  SERIAL.println(">>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+  if(ams5600.detectMagnet() == 0 ){
+    while(1){
+        if(ams5600.detectMagnet() == 1 ){
+            SERIAL.print("Current Magnitude: ");
+            SERIAL.println(ams5600.getMagnitude());
+            break;
+        }
+        else{
+            SERIAL.println("Can not detect magnet");
+        }
+        delay(1000);
+    }
+  }
+}
+
+void loop()
+{
+    Step(true);
+    SERIAL.println("Actual: " + String(convertRawAngleToDegrees(ams5600.getRawAngle()),DEC));
+    delay(2);
+}
+
+float convertRawAngleToDegrees(word newAngle)
+{
+  float retVal = newAngle * 0.087890625;
+  return retVal;
+}
+
+void Step(bool dir) {
+  if(go) {
+    if (dir) {
+      switch (step_number) {
+        case 0:
+          digitalWrite(STEPPER_PIN_1, HIGH);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, LOW);
+          break;
+        case 1:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, HIGH);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, LOW);
+          break;
+        case 2:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, HIGH);
+          digitalWrite(STEPPER_PIN_4, LOW);
+          break;
+        case 3:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, HIGH);
+          break;
+      }
+    } else {
+      switch (step_number) {
+        case 0:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, HIGH);
+          break;
+        case 1:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, HIGH);
+          digitalWrite(STEPPER_PIN_4, LOW);
+          break;
+        case 2:
+          digitalWrite(STEPPER_PIN_1, LOW);
+          digitalWrite(STEPPER_PIN_2, HIGH);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, LOW);
+          break;
+        case 3:
+          digitalWrite(STEPPER_PIN_1, HIGH);
+          digitalWrite(STEPPER_PIN_2, LOW);
+          digitalWrite(STEPPER_PIN_3, LOW);
+          digitalWrite(STEPPER_PIN_4, LOW);
+      }
+    }
+  }
+  step_number++;
+  actual_step++;
+  angle = angle + step_angle;
+  if (step_number > 3) {
+    step_number = 0;
+  }
+  if (angle > 360) {
+    angle = 0;
+  }
+  //Serial.println("Predicted: " + String(angle));
+}
